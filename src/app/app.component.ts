3c3b7e6d456f16d7a64454d44dc2305ee76cb9a6
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-07-11 16:03:31 +0530

    Project with navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'AngularFormTableApp';
-}
+export class AppComponent {}
